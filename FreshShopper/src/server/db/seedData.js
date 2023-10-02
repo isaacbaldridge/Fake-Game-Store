c58d015b61306b454d594fd51443615030445edb
isaacbaldridge <baldridgela@alumni.vcu>
2023-10-02 19:28:49 -0400

    sorders table seed data

---
+++ FreshShopper/src/server/db/seedData.js
@@ -0,0 +1,42 @@
+const users = [
+
+]
+
+const products = [
+
+]
+
+const orders = [
+    {
+        user_id: 1,
+        fulfilled: false
+    },
+    {
+        user_id: 2,
+        fulfilled: true
+    },
+    {
+        user_id: 3,
+        fulfilled: true
+    },
+    {
+        user_id: 3,
+        fulfilled: true
+    },
+    {
+        user_id: 3,
+        fulfilled: false
+    },
+    {
+        user_id: 4,
+        fulfilled: false
+    },
+    {
+        user_id: 5,
+        fulfilled: true
+    }
+]
+
+const order_products = [
+
+]
