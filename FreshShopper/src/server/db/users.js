b8d301949b38b2d86e5ac7623429928e6fac9a7e
isaacbaldridge <baldridgela@alumni.vcu.edu>
2023-10-10 13:58:30 -0400

    /db/users.js created, createUser sql query defined

---
+++ FreshShopper/src/server/db/users.js
@@ -0,0 +1,20 @@
+const db = require("./client")
+
+
+const createUser = async( { name, email, password, address, profilePic, isAdmin} ) => {
+    try {
+        const { rows: [ user ] } = await db.query(`
+        INSERT INTO users (name, email, password, address, profilePPic, isAdmin)
+        VALUES($1, $2, $3, $4, $5, $6)
+        RETURNING *
+        `, [name, email, password, address, profilePic, isAdmin])
+        return user
+    } catch (error) {
+        throw error
+    }
+}
+
+
+module.exports = {
+    createUser
+}
