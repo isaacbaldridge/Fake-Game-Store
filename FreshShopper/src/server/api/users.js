b438687b7fb0ce405b73ce6fee0cd9e981db4430
isaacbaldridge <baldridgela@alumni.vcu.edu>
2023-10-19 12:51:09 -0400

    created basic register api endpoint. add jwt later

--- FreshShopper/src/server/api/users.js
+++ FreshShopper/src/server/api/users.js
@@ -3,8 +3,9 @@ const usersRouter = express.Router()
 const chalk = require("chalk")
 const morgan = require("morgan")
 
-// ---- Loggin middleware -----
+// ---- Loggin middleware ---- //
 usersRouter.use(morgan("tiny"))
+
 
 const {
     createUser,
@@ -19,6 +20,31 @@ usersRouter.get("/", async (req, res, next) => {
         const users = await getAllUsers()
         res.send(users)
     } catch({name, message}) {
+        next({name, message})
+    }
+})
+
+// /api/users/register
+usersRouter.post("/register", async (req, res, next) => {
+    const { name, email, password, address, profilepic } = req.body
+    try {
+        if (!name || !email || !password) {
+            return res.status(401).send("Missing name, email, and/or password")
+        }
+
+        const user = await createUser({
+            name,
+            email,
+            password,
+            address,
+            profilepic 
+        })
+
+        if (user) {
+            res.send(user)
+        }
+
+    } catch ({name, message}) {
         next({name, message})
     }
 })
